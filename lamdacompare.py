02e9a6500af2936ca3fa1347a0f2d27b1ca82623
sdavid09 <shobin.david9@gmail.com>
2018-04-26 00:59:59 -0500

    New changes

--- lamdacompare.py
+++ lamdacompare.py
@@ -2,6 +2,7 @@ import boto3
 from decimal import Decimal
 import json
 import urllib
+import pprint
 
 print('Loading function')
 
@@ -46,16 +47,40 @@ def lambda_handler(event, context):
         # Calls Amazon Rekognition IndexFaces API to detect faces in S3 object 
         # to index faces into specified collection
         
-        response = index_faces(bucket, key, key2)
+        #response = index_faces(bucket, key, key2)
         
         # Commit faceId and full name object metadata to DynamoDB
         
    
 
         # Print response to console
-        print(response)
-
-        return response
+       # if response[1][0]['Similarity'] > 90 :# print only similarity 
+       #     print "MATCH!"   # 
+       
+       
+        imgs=[]
+        val=[]
+        s3 = boto3.resource('s3')
+        for bucket in s3.buckets.all():
+            for obj in bucket.objects.filter(Prefix='Building/entrance/img_entering/'):
+                imgs.append(obj.key)
+      
+        imgs.remove("Building/entrance/img_entering/") # remove home path so face comparison wont try to compare
+        #val = imgs[:]
+        #print imgs # list of all keys
+       # filter(lambda x: response[1][0]['Similarity'] > 90)
+        val = list( map ( lambda x: x.encode('utf8'), imgs)) # convert list of unicode to string
+        for x in val:
+            response = index_faces('eldimage', 'test.jpg', x)
+            print response
+            try:
+                if response[1][0]['Similarity'] > 90 :# print only similarity 
+                    print "MATCH!" 
+            except:
+                print "NOT MATCH!"# 
+       
+        
+        #return response
     except Exception as e:
         print(e)
         print("Error processing object {} from bucket {}. ".format(key, bucket))
